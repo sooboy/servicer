36c7d8f0b90ac434528a6266cfd2d6fc753eb56c
Regan <sooboy@foxmail.com>
2018-12-10 19:25:13 +0800

    finish

--- parallel.go
+++ parallel.go
@@ -1,1 +1,37 @@
 package servicer
+
+import (
+	"sync"
+)
+
+// Paralleler 并发执行Servicer
+type Paralleler interface {
+	Push(...Servicer)
+	Run()
+}
+
+// Parallel implements Paralleler
+type Parallel struct {
+	items []Servicer
+	wait  sync.WaitGroup
+}
+
+// Push  implements Paralleler
+func (p *Parallel) Push(items ...Servicer) {
+	if p.items == nil {
+		p.items = make([]Servicer, 0)
+	}
+	p.items = append(p.items, items...)
+}
+
+// Run implements Paralleler
+func (p *Parallel) Run() {
+	for i := 0; i < len(p.items); i++ {
+		p.wait.Add(1)
+		go func(num int) {
+			p.items[num].Run()
+			p.wait.Done()
+		}(i)
+	}
+	p.wait.Wait()
+}
--- servicer.go
+++ servicer.go
@@ -1,28 +1,48 @@
 package servicer
 
-type Function = func();
+import (
+	"log"
+	"time"
+)
 
-type Servicer  interface {
+// Function 函数别名
+type Function = func()
+
+// Servicer 运行容器
+type Servicer interface {
 	Run()
+	ID() string
 }
 
-// 基础函数
+// Service 基础函数
 type Service struct {
-	 name string 
-	 fn   Function
+	name string
+	fn   Function
 }
 
-func (this *Fun)Run(){
-	 this.fn()
+// Run implement Servicer
+func (s *Service) Run() {
+	s.fn()
 }
 
-// LogService
-type LogService struct {
+// ID implement Servicer
+func (s *Service) ID() string {
+	return s.name
+}
+
+// LogtimeService 基础输出
+type LogtimeService struct {
 	service Servicer
 }
 
-func (this *LogService)Run(){
-	
+// Run implement Servicer
+func (l *LogtimeService) Run() {
+	now := time.Now()
+	l.service.Run()
+	log.Println(l.service.ID(), "Runing Time is :", time.Now().Sub(now).String())
 }
 
-
+// ID implement Servicer
+func (l *LogtimeService) ID() string {
+	return l.service.ID() + "logTime "
+}
